1a7ed3e6c899964ec21b7f6f5146e5f31db051a7
Edubew <edubew98@gmail.com>
2022-06-11 10:23:13 +0300

    Add the edit task functionality

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ const addTask = (taskValue) => {
   listItem.innerHTML = `
   <input type="checkbox" class="checkbox"/>
   <span>${taskValue}</span >
-  <i class="fa-solid fa-ellipsis-vertical"></i>
+  <i class="fa-solid fa-ellipsis-vertical edit-btn"></i>
   <i class="fa-solid fa-trash-can delete-btn"></i>
   `;
   tasks.appendChild(listItem);
@@ -42,9 +42,40 @@ const addTask = (taskValue) => {
   const store = new Task(taskValue, false, checkbox.length - 1);
   listArray.push(store);
   localStorage.setItem('list', JSON.stringify(listArray));
+
+  // Edit functionality
+  const editBtn = document.querySelectorAll('.edit-btn');
+  editBtn.forEach((i) => {
+    i.addEventListener('click', () => {
+      editTask(listItem, i.previousElementSibling);
+    });
+  });
 };
 
-// add event listener
+const editTask = (listItem, task) => {
+  const editInput = document.createElement('input');
+  editInput.type = 'text';
+  editInput.className = 'editInput';
+  editInput.value = task.textContent;
+  listItem.replaceChild(editInput, task);
+  editInput.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+      const listItemContainers = document.querySelectorAll('.listItem');
+      const storedData = JSON.parse(localStorage.getItem('list'));
+      for (let i = 0; i < listItemContainers.length; i += 1) {
+        if (listItemContainers[i].classList.contains('checkContainer')) {
+          storedData[i].description = editInput.value;
+          localStorage.setItem('list', JSON.stringify(storedData));
+        }
+      }
+      editInput.parentElement.classList.remove('checkContainer');
+      listItem.replaceChild(task, editInput);
+      task.textContent = editInput.value;
+    }
+  });
+};
+
+// add task event listener
 inputField.addEventListener('keypress', (e) => {
   if (e.key === 'Enter' && inputField.value) {
     e.preventDefault();
